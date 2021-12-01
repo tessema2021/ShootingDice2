2eed87ea9ca6f21b5a9b8a0d8893e6869d7c9a62
tessema2021 <ninafuchere@gmail.com>
2021-12-01 08:19:00 -0600

    onehigherplayer done

--- OneHigherPlayer.cs
+++ OneHigherPlayer.cs
@@ -5,24 +5,21 @@ namespace ShootingDice
     // TODO: Complete this class
 
     // Override the Play method to make a Player who always roles one higher than the other player
-    public class OneHigherPlayer
+    public class OneHigherPlayer : Player
     {
-        //         public override void play(Player other)
-        //         {
-        //             int otherRoll = other.Roll();
-        //             int myRoll = otherRoll + 1;
-        //             Console.WriteLine($"{Name} rolls a {myRoll}");
-        //             Console.WriteLine($"{other.Name} rolls a {otherRoll");
-        // };
 
+        public override void Play(Player other)
 
+        {
 
+            int otherRoll = other.Roll();
+            int myRoll = otherRoll + 1;
+            Console.WriteLine($"{Name} rolls a {myRoll}");
+            Console.WriteLine($"{other.Name} rolls a {otherRoll}");
+            Console.WriteLine($"{Name} Wins!");
+
+        }
 
     }
-
-
-
-
-
-}
+};
 
--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace ShootingDice
             SmackTalkingPlayer player1 = new SmackTalkingPlayer();
             player1.Name = "Bob";
 
-            Player player2 = new Player();
+            OneHigherPlayer player2 = new OneHigherPlayer();
             player2.Name = "Sue";
 
             player2.Play(player1);
